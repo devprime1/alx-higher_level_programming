35f567d871163b3003704a510edfab697e16b6c7
I.T. Guy <chibuisiukegbu@gmail.com>
2022-12-09 18:58:11 +0000

    Done

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,18 +1,21 @@
 #include "lists.h"
+
 /**
- * print_dlistint - Prints all the nodes of ll
- * @h: Is the pointer to node structure
- * Return: the amount of nodes
+ * print_dlistint - Prints all the elements of a dlistint_t list.
+ * @h: The head of the dlistint_t list.
+ *
+ * Return: The number of nodes in the list.
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-	int node_count = 0;
+	size_t nodes = 0;
 
-	while (h != NULL)
+	while (h)
 	{
+		nodes++;
 		printf("%d\n", h->n);
 		h = h->next;
-		node_count++;
 	}
-	return (node_count);
+
+	return (nodes);
 }
--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,8 +1,8 @@
-fndef _LISTSH_
-#define _LISTH_
+#ifndef LISTS_H
+#define LISTS_H
 
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * struct dlistint_s - doubly linked list
@@ -11,7 +11,6 @@ fndef _LISTSH_
  * @next: points to the next node
  *
  * Description: doubly linked list node structure
- * for ALX  projects
  */
 typedef struct dlistint_s
 {
@@ -19,7 +18,6 @@ typedef struct dlistint_s
 	struct dlistint_s *prev;
 	struct dlistint_s *next;
 } dlistint_t;
-
 
 size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
@@ -31,4 +29,4 @@ int sum_dlistint(dlistint_t *head);
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
 
-#endifi
+#endif /* LISTS_H */
